498f8677700d95db830657c91dfac4e7a1039c67
Eslam Dyab <77991372+eslamdyab21@users.noreply.github.com>
2021-10-02 20:17:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -769,7 +769,7 @@ with st.beta_expander("Display well's status histograms",False):
     st.markdown(get_binary_file_downloader_html('Wells.xlsx', 'Well count and the main well data'), unsafe_allow_html=True)
     #--------------------------------------------------------------
     from plot_wells_status_purpose import plot_wells_status_purpose_content
-    plot_wells_status_purpose_content.plot(df_wells,userValue,final_directory)
+    plot_wells_status_purpose_content().plot(df_wells,userValue,final_directory)
 #=======================================================================================================================================================
 dfcum = df_newcSUM.copy()
 
